21e5a73a06288f37a1aefe359891fbf77115bdb4
Adolfo Correa <adolfo.correa@lamcso.coppe.ufrj.br>
2018-12-26 07:00:29 -0200

    command line verifyer

--- image_module.py
+++ image_module.py
@@ -7,10 +7,28 @@ Created on Thu Dec 20 23:49:13 2018
 from PIL import Image
 from numpy import array
 from numpy import int64
+from sys import exit
 
 
 class Filter:
     def __init__(self,img,path):
+        """
+
+        Parameters
+        ----------
+        img : Image.open() reference
+            It's a Image object from pillow library
+
+        path : String
+            It`s the path to the file
+
+
+        Set up
+        -------
+            self.img = img
+            self.img_out = []
+            self.path = path
+        """
         self.img = img
         self.img_out = []
         self.path = path
@@ -21,6 +39,17 @@ class Filter:
     def parameters_needed(): pass
 
     def _save(self,filename,fields):
+        """Save the changed images
+
+        Parameters
+        ----------
+        filename : list of strings
+            It contains the strings with differents filenames
+
+        fields : list of variables
+            It contains the variables that will be used to make the filenames
+
+        """
         for img,fn in zip(self.img_out,filename):
             img.save(fn.format(*fields))
         return 0
@@ -36,15 +65,14 @@ class Blur(Filter):
 
         Parameters
         ----------
-        img : ImageType
+        img : Image object from Pillow library
             It's a ImageType object
 
 
         Returns
         -------
-        
-        
-
+        blur : Image object from Pillow library
+            It contains the changed image
         """
 
         blur = self.__blur_filter()
@@ -56,13 +84,20 @@ class Blur(Filter):
         return blur
 
     def set_parameters(self,parameters_list):
+        """Establish the input parameters of the filter
+        """
         self.radius, self.weight = parameters_list
 
     @staticmethod
     def parameters_needed():
+        """Let the user know the required parameters in orther
+        to apply the filter
+        """
         return ('radius','weight')
 
     def save(self):
+        """Save the file after to apply the filter
+        """
         filename = ['{0}-blur-radius{1}-weight{2}.{3}']
         fn, fext = self._filename_separator()
         fields = (fn,self.radius,self.weight,fext)
@@ -103,10 +138,34 @@ class Blur(Filter):
             rgb = rgb.astype(int64)
               
             blur_color = tuple(rgb)
-              
+            
+            #blur_color = self.__blur_pane(x,y,rgb,pix_count, width, height)
             img_blur.putpixel((x, y), blur_color)
     
         return img_blur
+
+    
+    def __blur_pane(self,x,y,rgb,pix_count, width, height):
+            for xiradius in range(-self.radius,self.radius+1):
+              for yiradius in range(-self.radius,self.radius+1):
+                if x + xiradius >= 0 and x + xiradius < width:
+                  if y + yiradius >= 0 and y + yiradius < height:
+                    pix_count += int(1)
+                    if xiradius != 0 or yiradius !=0:
+                      rgb += array(self.img.getpixel((x+xiradius, y+yiradius)))
+                    else:
+                      rgb += self.weight*array(self.img.getpixel((x+xiradius, y+yiradius)))
+
+
+                    
+            rgb = rgb/self.__denominator(pix_count,self.weight)
+              
+            rgb = rgb.round()
+            rgb = rgb.astype(int64)
+            blur_color = tuple(rgb)
+        
+            return blur_color
+
 
     @staticmethod
     def __denominator(pix_count,weight):
@@ -137,10 +196,15 @@ class RGB_split(Filter):
         return self.img_out
 
     def set_parameters(self,parameters_list):
+        """Establish the input parameters of the filter
+        """
         return 0
 
     @staticmethod
     def parameters_needed():
+        """Let the user know the required parameters in orther
+        to apply the filter
+        """
         return []
 
     def save(self):
@@ -203,6 +267,8 @@ class ImageType:
 class RGBImage(ImageType):
 
     def __init__(self, img_ref,path):
+        """Store the available filters filters for the specific Image type
+        """
         super( RGBImage, self ).__init__(img_ref,path)
         self.filters =\
                 {\
@@ -211,10 +277,14 @@ class RGBImage(ImageType):
                 }
 
     def available_filters(self):
+        """Return the list of the filter available for the specific Image type
+        """
         return self.filters.keys()
 
 class SingleChannelImage(ImageType):
     def __init__(self, img_ref,path):
+        """Store the available filters filters for the specific Image type
+        """
         super( SingleChannelImage,self ).__init__(img_ref,path)
         self.filters =\
                 {\
@@ -222,24 +292,95 @@ class SingleChannelImage(ImageType):
                 }
 
     def available_filters(self):
+        """Return the list of the filter available for the specific Image type
+        """
         return self.filters.keys()
+
+class cli_verify():
+
+    aval_flags = ('-rgb','-blur')
+    flags_filters= {'-rgb':'RGB split','-blur':'Blur'}
+    chosen_flag = ''
+
+    def __init__(self, argv):
+        self.argv = argv
+        self.flag = self.exist_a_flag()
+        self.filename = self.get_filename()
+        self.exist_file()
+        if self.exist_a_flag():
+            self.existing_flag()
+                
+
+    def chosen_command(self):
+        return self.flags_filters[self.chosen_flag]
+
+        
+    def exist_a_flag(self):
+        if self.argv[0][0] == '-':
+            self.chosen_flag=self.argv[0][0]
+            self.parameters = map(lambda x : int(x) ,self.argv[2:])
+            return True
+        else:
+            return False
+
+    def exist_file(self):
+       try:
+           fn = open(self.filename, 'r')
+       except FileNotFoundError:
+            print('File not found')
+            exit(1)
+            return False
+
+       return True
+
+    def get_filename(self):
+        if self.exist_a_flag() == True:
+            return self.argv[1]
+        else:
+            return self.argv[0]
+
+    def existing_flag(self):
+        for fgs in self.aval_flags:
+            if self.argv[0] == fgs:
+                self.chosen_flag = self.argv[0]
+                finded = True
+                break
+            else:
+                finded = False
+        if finded:
+            return True
+        else:
+                print('Wrong flag')
+                exit(1)
+                return False
+
+
 
 def main(argv):
 
     print('This is an Image App', end = '\n\n')
 
-    img_file = Image.open(argv[0])
+    arguments = cli_verify(argv)
+
+    img_file = Image.open(arguments.filename)
+    print('362 {}'.format(arguments.filename))
     img_mode = img_file.mode
 
     print('Image type: {0}'.format(img_mode), end = '\n\n')
 
-    image = ImageType.factory(img_mode, img_file,argv[0])
+    image = ImageType.factory(img_mode, img_file,arguments.filename)
+
     
-    avail_filters = display_available_filters(image)
+    if not arguments.flag:
+        avail_filters = display_available_filters(image)
 
-    chosen_filter = choose_filter(avail_filters)
+        chosen_filter = choose_filter(avail_filters)
 
-    parameters_list = ask_for_parameters(image, chosen_filter)
+        parameters_list = ask_for_parameters(image, chosen_filter)
+
+    else:
+        chosen_filter = arguments.chosen_command()
+        parameters_list = arguments.parameters
 
     image.filters[chosen_filter].set_parameters(parameters_list)
 
